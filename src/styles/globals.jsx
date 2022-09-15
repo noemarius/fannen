f7fd510cca2869bd8f57a7f1d57bce9bae3a7b0c
Ricky352 <lopes.ricky97@gmail.com>
2022-09-15 15:21:58 +0200

    layout update

--- src/styles/globals.jsx
+++ src/styles/globals.jsx
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+* {
+    box-sizing: border-box;
+    outline: 1px solid black !important;
+}
+
 body {
     background-color: rgb(211, 211, 211);
     width: 100%;
