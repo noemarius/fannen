f71ee800df4fe33864e568c79e6b2fc0ca1778f1
Ricky352 <lopes.ricky97@gmail.com>
2022-09-27 09:42:58 +0200

    update team page

--- src/components/Member.jsx
+++ src/components/Member.jsx
@@ -1,21 +1,38 @@
 import Image from 'next/image'
 import styled from 'styled-components'
+import { device } from './Device'
 import { Text } from './Text'
+import { Title } from './Title'
 
 const MemberContainer = styled.div`
     align-items: center;
     display: flex;
-    flex-direction: ${props => props.direction || 'row'};
+    flex-direction: ${props => props.direction || 'column-reverse'};
     gap: 20px;
-    justify-content: space-between;
-    margin: 20px 0px;
+    justify-content: center;
+    margin: 40px 0px 0px 0px;
+
+    @media ${device.tablet} {
+        flex-direction: ${props => props.directionTablet || 'row'};
+    }
+`
+
+const TextContainer = styled.div`
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    flex: 3;
+    gap: 12px;
 `
 
 export function Member(props) {
     return (
         <>
-            <MemberContainer direction={props.direction}>
-                <Text text={props.text} width="60%" />
+            <MemberContainer direction={props.direction} directionTablet={props.directionTablet}>
+                <TextContainer>
+                    <Title title={props.title} size={props.size} />
+                    <Text text={props.text} width="100%" />
+                </TextContainer>
                 <Image
                     src={props.image}
                     height={props.height}
--- src/pages/team.jsx
+++ src/pages/team.jsx
@@ -13,6 +13,8 @@ export default function Team() {
                     <Container margin="28px 0px">
                         <Title title="Meet The Team" size="44px" />
                         <Member
+                            title="Ricardo Lopes:"
+                            size="20px"
                             text="Ricardo Lopes is responsible for our front end part of Fannen. He is 25 years old, has always been passionate about the web and got into development a couple of months ago. He used Next.js coupled with styled-components to render what you can see on the website."
                             image="/sample.jpeg"
                             height="220"
@@ -20,25 +22,31 @@ export default function Team() {
                         />
 
                         <Member
+                            title="Zakariya Bouaroua:"
+                            size="20px"
                             text="Zakariya developed the backend part of the fannen.lu site with php and the Laravel framework. Passionate about code and the logic linked to it, Zakariya brought his meticulousness and his sense of logic to develop the database as well as the backend structure of this site."
                             image="/sample.jpeg"
                             height="220"
                             width="150"
-                            direction="row-reverse"
+                            directionTablet="row-reverse"
                         />
 
                         <Member
+                            title="Ricardo Lopes:"
+                            size="20px"
                             text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus quam voluptatibus aliquid dolorum nemo quas a sed assumenda perferendis debitis.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus quam voluptatibus aliquid dolorum nemo quas a sed assumenda perferendis debitis."
                             image="/sample.jpeg"
                             height="220"
                             width="150"
                         />
                         <Member
+                            title="Noé Marius:"
+                            size="20px"
                             text="Noé Marius, is a 32 year old tech enthousiast. He is a System Engineer recently turned fullstack developer, with almost a decade of experience in the IT field, he is managing the Fannen team and the app, he also works on the frontend, backend as well as the infrastructure to host and run the application."
                             image="/sample.jpeg"
                             height="220"
                             width="150"
-                            direction="row-reverse"
+                            directionTablet="row-reverse"
                         />
                     </Container>
                 </Card>
